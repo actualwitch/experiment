68920aa0aa2d22f66a1b46628d5c8d06ce16c35d
Adelaide あで Fisher <noreply@actualwitch.me>
2025-02-12 00:10:49 +0100

    begone, hack

--- scripts/fix.bun.ts
+++
@@ -1,6 +0,0 @@
-
-let text = await Bun.file("node_modules/jsdom/lib/jsdom/living/xhr/XMLHttpRequest-impl.js").text();
-
-text = text.replace(`require.resolve("./xhr-sync-worker.js")`, "null");
-
-Bun.write("node_modules/jsdom/lib/jsdom/living/xhr/XMLHttpRequest-impl.js", text);
